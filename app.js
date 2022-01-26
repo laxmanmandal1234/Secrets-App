951dae11cc3945ce383efa31039d78a8c79b4728
Laxman Mandal <laxmanmandal1234@gmail.com>
2022-01-26 23:17:23 +0530

    Level 3 - Hashing password with md5

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
 //jshint esversion:6
+require('dotenv').config();
+//require dotenv module as early as possible
 
 const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5'); //md5 is a JavaScript function for hashing messages
 
 const app = express();
-mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
 
-const secret = "IknowWhatYouAreThinkingYesThisIsMyLongStringForEncryptionHAHAHA";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"]});
+//to use the SECRET string defined in .env use process.env.VARIABLE_NAME
+//userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"]});  //not needed if you are using hash function
 
 //create model only after applying plugin
 const User = mongoose.model("User", userSchema);
@@ -38,59 +44,47 @@ app.get("/login", function(req, res) {
 
 //REGISTER
 app.post("/register", function(req, res) {
-  const newUser = User({
-    email: req.body.username,
-    password: req.body.password
-  });
+    const newUser = User({
+      email: req.body.username,
+      password: md5(req.body.password)
+    });
 
-  newUser.save(function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
-  });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
+
 });
-
 
 //LOGIN
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne(
     {email: username},
     function(err, foundUser) {
-      if(err) {
+      if (err) {
         console.log(err);
       } else {
-        if(foundUser) {
-          if(foundUser.password === password) {
-            console.log("You are successfully logged in");
-            res.render("secrets");
-          }
-        } else {
-          console.log("Either email or password is incorrect");
-        }
+        if (foundUser) {
+            if(foundUser.password === password) {
+              console.log("You are successfully logged in");
+              res.render("secrets");
+            } else {
+              console.log("Either email or password is incorrect");
+            }
       }
     }
-  );
+  });
 });
 
 //NOTE:
 // During save(), documents are encrypted and then signed. During find(), documents
 // are authenticated and then decrypted
-
-
-
-
-
-
-
-
-
-
-
 
 
 
