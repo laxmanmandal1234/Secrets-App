09d0916254daf035742af8e6f08647375dbab2b2
Laxman Mandal <laxmanmandal1234@gmail.com>
2022-01-26 23:22:25 +0530

    Level 3 - Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-// const encrypt = require('mongoose-encryption'); //this was used for level 2 - encryption
+
 // const md5 = require('md5'); //md5 is a JavaScript function for hashing messages
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
