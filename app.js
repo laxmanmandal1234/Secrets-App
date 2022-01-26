7d6e2924ba0f727e85e8c6984112ef569d43cdea
Laxman Mandal <laxmanmandal1234@gmail.com>
2022-01-26 23:21:07 +0530

    Level 3 - Hashing and Salting with bcrypt

--- app.js
+++ app.js
@@ -6,7 +6,10 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const md5 = require('md5'); //md5 is a JavaScript function for hashing messages
+// const encrypt = require('mongoose-encryption'); //this was used for level 2 - encryption
+// const md5 = require('md5'); //md5 is a JavaScript function for hashing messages
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 const app = express();
 mongoose.connect("mongodb://localhost:27017/userDB", {
@@ -44,9 +47,13 @@ app.get("/login", function(req, res) {
 
 //REGISTER
 app.post("/register", function(req, res) {
+  myPlaintextPassword = req.body.password;
+  bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
     const newUser = User({
       email: req.body.username,
-      password: md5(req.body.password)
+      password: hash    // Store hash in your password DB.
+      // password: req.body.password //this was used for level 1 and 2
+      //password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -56,13 +63,15 @@ app.post("/register", function(req, res) {
         res.render("secrets");
       }
     });
-
+  });
 });
 
 //LOGIN
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  // const password = req.body.password; //this was used for level 1 and 2
+  //const password = md5(req.body.password);
+  myPlaintextPassword = req.body.password;
 
   User.findOne(
     {email: username},
@@ -71,12 +80,14 @@ app.post("/login", function(req, res) {
         console.log(err);
       } else {
         if (foundUser) {
-            if(foundUser.password === password) {
+          bcrypt.compare(myPlaintextPassword, foundUser.password, function(err, result) {
+            if(result == true) {
               console.log("You are successfully logged in");
               res.render("secrets");
             } else {
               console.log("Either email or password is incorrect");
             }
+        });
       }
     }
   });
