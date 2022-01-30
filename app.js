30ae4d49052091355123d7440ad50b226b9bef6b
Laxman Mandal <laxmanmandal1234@gmail.com>
2022-01-30 23:47:20 +0530

    Changes port to listen on Heroku server

--- app.js
+++ app.js
@@ -177,7 +177,10 @@ app.post("/submit", function(req, res) {
 // are authenticated and then decrypted
 
 
-
-app.listen(3000, function() {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
